25d5c9cbbe918089ca0fcfa5c7c1967fbbadcbc0
Luka <luka.luka.luka93@gmail.com>
2023-12-18 14:13:18 -0800

    article preview

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const share = document.querySelector(".share");
+
+const body = document.querySelector("body")
+
+const shareIcons = document.querySelector(".share-icons");
+
+const nameAvatar = document.querySelector(".name-avatar");
+
+const popUp = document.querySelector(".popup-desktop");
+
+            
+
+
+
+
+
+    
+
+
+
+
