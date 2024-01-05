d9755d2a3541ea9289c8e676ce4f9127c1fd8d3d
Luka <luka.luka.luka93@gmail.com>
2024-01-04 18:05:27 -0800

    Changed to use img tag instead of css

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 const share = document.querySelector(".share");
 
-const body = document.querySelector("body")
+const shareIconsMob = document.querySelector(".share-icons");
 
-const shareIcons = document.querySelector(".share-icons");
+const shareIconsDesk = document.querySelector(".popup-desktop");
 
-const nameAvatar = document.querySelector(".name-avatar");
-
-const popUp = document.querySelector(".popup-desktop");
-
-            
+const nameAvatar = document.querySelector(".name-avatar")
 
 
-
-
-
-    
-
-
+share.addEventListener("click", function(){
+    shareIconsMob.classList.remove("hide");
+    nameAvatar.classList.add("hide");
+    if(window.innerWidth > 1440){
+        shareIconsMob.classList.add("hide");
+        nameAvatar.classList.remove("hide");
+    }
+})
 
 
