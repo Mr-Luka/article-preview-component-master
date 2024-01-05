fc8cce5cc350fe2ebb9ab04fe348155b7f1faf0a
Luka <luka.luka.luka93@gmail.com>
2024-01-05 15:07:39 -0800

    Everything fixed

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ const shareIconsDesk = document.querySelector(".popup-desktop");
 
 const nameAvatar = document.querySelector(".name-avatar")
 
+const triangle = document.querySelector(".triangle");
+
 
 share.addEventListener("click", function(){
     shareIconsMob.classList.remove("hide");
@@ -13,7 +15,29 @@ share.addEventListener("click", function(){
     if(window.innerWidth > 1440){
         shareIconsMob.classList.add("hide");
         nameAvatar.classList.remove("hide");
+        triangle.classList.remove("hide");
+        shareIconsDesk.classList.remove("hide");
+    } else {
+    shareIconsMob.classList.remove("hide");
+        nameAvatar.classList.add("hide");
+        triangle.classList.add("hide");
+
+        shareIconsDesk.classList.add("hide");
     }
-})      
+})
+
+window.addEventListener("resize", function(){
+    if(window.innerWidth <= 1440){
+        shareIconsMob.classList.remove("hide");
+        nameAvatar.classList.add("hide");
+        triangle.classList.add("hide");
+
+        shareIconsDesk.classList.add("hide");
+    } else {
+        shareIconsMob.classList.add("hide");
+        nameAvatar.classList.remove("hide");
+        
+    }
+});
 
 
